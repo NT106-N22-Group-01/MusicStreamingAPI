5b60d8db445b84781c58a00916451f67763da28c
Khanh Hoang <bacdau852@gmail.com>
2023-06-07 01:32:38 +0700

    Implement daemon for nix

---
+++ src/daemon/daemon_nix.go
@@ -0,0 +1,13 @@
+//go:build !windows
+// +build !windows
+
+package daemon
+
+import "syscall"
+
+// StopSignals contains all the signals which will make our daemon remove its pidfile
+var StopSignals = []syscall.Signal{
+	syscall.SIGINT,
+	syscall.SIGKILL,
+	syscall.SIGTERM,
+}
