28bcd10ffe8e15f6d3ba9c8abcf519d2abf89d1c
tuilakhanh <bacdau852@gmail.com>
2023-06-12 02:29:59 +0700

    set default view to 0

--- sqls/migrations/006_listens_count.sql
+++ sqls/migrations/006_listens_count.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-alter table tracks add column listens_count integer;
+alter table tracks add column listens_count integer default 0;
 
 -- +migrate Down
 alter table tracks drop column listens_count;
